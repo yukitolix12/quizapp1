d904ee34183c03d57aa4d6cf9b392046f959df99
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-04 13:13:42 +0900

    make answers for question

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const questions = [
     {
         question: "Which is largest animal in the world?",
-        
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
     }
 ]
