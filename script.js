248bf16c87a6acd6dddecf717149758fc9759a70
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-04 13:22:26 +0900

    add questions and answers

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ const questions = [
             { text: "Elephant", correct: false},
             { text: "Giraffe", correct: false},
         ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ]
     }
 ]
