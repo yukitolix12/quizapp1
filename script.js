67c9fb5bce42be617003abac901a9e5b93ac5a5f
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-04 13:34:03 +0900

    add function to startQuiz

--- script.js
+++ script.js
@@ -34,3 +34,10 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
